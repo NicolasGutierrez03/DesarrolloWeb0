71b71f298e7cc651b4d3c229b2a36b916d39a0c7
NicolasGutierrez0 <nicolas.gutierrez6@mail.udp.cl>
2025-09-01 20:21:26 -0400

    Intregracion js header y footer.

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+getMenu(`footer`)
+        function getMenu(_id) {
+            const footer = document.getElementById(_id);
+            menu.innerHTML = `
+            <p>&copy; 2025 Casino website</p>
+            `
+        }
---
+++ js/menuPrincipal.js
@@ -0,0 +1,29 @@
+        getMenu(`menuPrincipal`)
+        function getMenu(_id) {
+            const menu = document.getElementById(_id);
+            menu.innerHTML = `
+            <div class="logo-nombre">
+                <img class="logo" src="img/Logo.jpg" alt="Logo">
+                <h1 class="nombre">Casino UDP</h1>
+            </div>
+            <div class="mynav">
+                <ul class="nav nav-pills justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Perfil.html">Perfil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contacto.html">Contactanos</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mylogin">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">iniciar</button>
+                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#registerModal">registrar</button>
+                </div>
+            </div>`;
+        }
